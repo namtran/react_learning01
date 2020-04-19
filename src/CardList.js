ca1168c2d169740a7f51a80f75d5fa5719592cbd
Nam <vannamtran@gmail.com>
2020-04-19 07:53:38 +0700

    create CardList, Card component display list of robots

---
+++ src/CardList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) =>
+{
+	{
+		/*
+		This is nore clearer way
+		*/
+	}
+	const CardArray = robots.map((user, i) => {
+		return ( 
+			<Card 
+				key = {i}
+				id = {robots[i].id}
+				name = {robots[i].name}
+				email = {robots[i].email}
+				/>
+
+		);
+	});
+
+	return (
+		<div>
+		{CardArray}
+		</div>
+		);
+	
+
+	{
+	/* This is another way (shorter display)
+	return(
+	<div>
+		{
+		robots.map((user,i) => {
+			return (
+				<Card 
+					key={i}
+					id = {robots[i].id}
+					name = {robots[i].name}
+					email = {robots[i].email}
+				/>
+				);
+			})
+		}
+	</div>
+	);
+	*/
+	}
+}
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+{
+	id:'1',
+	username: 'id1',
+	name:'John 01',
+	email:'john01@gmail.com'
+},
+{
+	id:'2',
+	username: 'id2',
+	name:'John 02',
+	email:'john02@gmail.com'
+},
+{
+	id:'3',
+	username: 'id3',
+	name:'John 03',
+	email:'john03@gmail.com'
+},
+{
+	id:'4',
+	username: 'id4',
+	name:'John 04',
+	email:'john04@gmail.com'
+},
+{
+	id:'5',
+	username: 'id5',
+	name:'John 05',
+	email:'john05@gmail.com'
+},
+{
+	id:'6',
+	username: 'id6',
+	name:'John 06',
+	email:'john06@gmail.com'
+},
+{
+	id:'7',
+	username: 'id7',
+	name:'John 07',
+	email:'john07@gmail.com'
+}
+];
