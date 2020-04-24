ffbbbfaa8f869d6e639dc1af4757c33606d6e6ff
Nam <vannamtran@gmail.com>
2020-04-24 09:48:08 +0700

    add redux logger and start backend

---
+++ back_end/node.js
@@ -0,0 +1,5 @@
+const a = 4;
+const b = 5;
+setTimeout((100) => {
+	console.log(a + b);
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
@@ -9,7 +9,11 @@ import * as serviceWorker from './serviceWorker';
 import { searchRobotsReducer } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobotsReducer)
+import { createLogger } from 'redux-logger';
+
+
+const logger = createLogger();
+const store = createStore(searchRobotsReducer, applyMiddleware(logger));
 
 ReactDOM.render(
 	<Provider store={store}>
