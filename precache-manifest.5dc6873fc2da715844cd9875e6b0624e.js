f2f31a761e7cf0fdba8fa5d25530567098e9e598
Nam <vannamtran@gmail.com>
2020-04-24 18:30:21 +0700

    Updates

---
+++ precache-manifest.5dc6873fc2da715844cd9875e6b0624e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9b2f6233d1a0708f8854632b31c776",
+    "url": "/react_learning01/index.html"
+  },
+  {
+    "revision": "2e55e568087dad264fb7",
+    "url": "/react_learning01/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "746a56ff9d47fc01b093",
+    "url": "/react_learning01/static/css/main.9b1c2b26.chunk.css"
+  },
+  {
+    "revision": "2e55e568087dad264fb7",
+    "url": "/react_learning01/static/js/2.2e6eec0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_learning01/static/js/2.2e6eec0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746a56ff9d47fc01b093",
+    "url": "/react_learning01/static/js/main.080a033c.chunk.js"
+  },
+  {
+    "revision": "146a78a287e614ce8ece",
+    "url": "/react_learning01/static/js/runtime-main.348843c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_learning01/static/media/SEGA.12a3db8b.woff"
+  }
+]);
